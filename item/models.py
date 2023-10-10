0303fff9a13d276102a2581a951386e78be36a26
Buhle Ntsondwa <ntsondwabb101@gmail.com>
2023-10-10 22:24:18 +0200

    Updated item models

--- item/models.py
+++ item/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Catergories'
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
